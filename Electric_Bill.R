0467e9b627108df669192d6dc438a859c06e32b1
Jonathan Hercules <jherc127@gmail.com>
2020-02-21 15:51:29 -0500

    Update Electric_Bill.R

--- Electric_Bill.R
+++ Electric_Bill.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(scales)
 library(lubridate)
 
+
 # Electric bill data csv file - file = your path 
 data = read.csv(file = "C:/Users/alcir/Downloads/electric bill.csv")
 # Get rid of Nan coloumns
@@ -14,6 +15,6 @@ data$Mnbtwu = data$`Total kwh` * .00341
 data$Date = as.Date(data$Date, format("%m/%d/%Y"))
 data = data[!duplicated(data$Date),]
 # Plot graph
-ggplot(data, aes(data$Date, data$Mnbtwu)) + geom_point(color ="blue",alpha=0.5) + scale_x_date(labels = year(data$Date)) +
-  theme_minimal() + scale_x_date() + xlab("Date") + ylab("MMBTU") + ggtitle("Total MMBTU in the last 2 years") + theme(plot.title = element_text(hjust = 0.5))
+ggplot(data, aes(data$Date, data$Mnbtwu)) + geom_line(alpha=0.5)  +
+  theme_grey() + scale_x_date() + xlab("Date") + ylab("MMBTU") + ggtitle("Total MMBTU in the last 2 years") + theme(plot.title = element_text(hjust = 0.5))
 
